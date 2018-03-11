43213f3c8d8d9719b1cfe5018827303dc4e97494
AquaticNewKeyHack <aaronquaday@gmail.com>
2018-03-10 17:59:22 -0800

    simple taskContentProvider OnCreate() and registration in manifest

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -25,7 +25,7 @@ import android.support.annotation.NonNull;
 // TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-
+        private static TaskDbHelper mDbHelper;
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
     In this case, you’re working with a SQLite database, so you’ll need to
@@ -35,7 +35,10 @@ public class TaskContentProvider extends ContentProvider {
     public boolean onCreate() {
         // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
-
+        if (null == mDbHelper){
+            mDbHelper = new TaskDbHelper(getContext());
+            return true;
+        }
         return false;
     }
 
